367bcf0c848fd962913a836a434cec78b0da7d8d
Dragon <dragonishare@gmail.com>
2018-05-07 18:16:07 +0800

    counter demo

---
+++ admin/src/components/about/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div className="About">
+                <h1>About Page</h1>
+                <p>Did you get here via Redux?</p>
+            </div>
+        );
+    }
+}
+
+export default About;
--- admin/src/redux/modules/index.js
+++ admin/src/redux/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
+import counter from './counter';
 
 const rootReducer = combineReducers({
-  router: routerReducer
+  router: routerReducer,
+  counter
 });
 
 export default rootReducer;
