463af90588e90e8557cc6a164f73b913a975f5fd
Dragon <dragonishare@gmail.com>
2018-05-07 15:56:46 +0800

    prettier插件配置代码格式化参数--no-bracket-spacing

--- admin/src/index.js
+++ admin/src/index.js
@@ -5,7 +5,5 @@ import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+    <App />,    document.getElementById("root"));
 registerServiceWorker();
